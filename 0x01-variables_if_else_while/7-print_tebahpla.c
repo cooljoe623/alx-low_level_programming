15ef201c9f27d54b7f59dbbbba8f29399450b41a
Your Name <you@example.com>
2023-03-17 05:53:52 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block of code
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
