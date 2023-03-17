e32c1df3f8e536a12a6ccf811125df3255242b8b
Your Name <you@example.com>
2023-03-17 05:48:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - executions begins here
+ * program prints all single digit numbers of base 10 starting from 0
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+
