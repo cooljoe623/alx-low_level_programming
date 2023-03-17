5b38764a4d84a46f4b406ba59c9db60a3d10897f
Your Name <you@example.com>
2023-03-17 05:39:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main where the execution of this program starts
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
