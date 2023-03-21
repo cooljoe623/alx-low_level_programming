8062c9c3e5dfc9979892b6dc5313ba75d8341394
Your Name <you@example.com>
2023-03-21 04:52:01 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @i: int type number
+ * @j: int type number
+ * Return:0 (success
+ */
+
+
+int add(int i, int j)
+{
+return (i + j);
+}
