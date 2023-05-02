72d8ced3a75c6e74b905dea94c02b434456869d6
Your Name <you@example.com>
2023-05-02 07:30:14 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,8 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp;
-	int i = 0;
+	listint_t *node, *temp;
+        unsigned int i = 0;
 
 	if (*head == NULL && idx != 0)
 		return (NULL);
@@ -24,17 +24,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (temp == NULL)
 		return (NULL);
 	}
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
 		return (NULL);
-	new_node->n = n;
+	node->n = n;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		node->next = *head;
+		*head = node;
+		return (node);
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	node->next = temp->next;
+	temp->next = node;
+	return (node);
 }
