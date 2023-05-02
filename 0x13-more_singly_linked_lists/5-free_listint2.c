4410475a16679bd7578efdcf6ac36bd650cdd1b8
Your Name <you@example.com>
2023-05-02 07:08:18 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,13 +4,17 @@
 /**
  * free_listint2 - Frees a list.
  * @head: Address of the first node of a list.
+ * free_listint2 - Frees a listint_t list and sets the head to NULL
+ * @head: Pointer to the head of the list
+ *
+ * Return: Nothing
  **/
 
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == NULL *head == NULL)
+	if (head == NULL )
 		return;
 	while (*head != NULL)
 	{
