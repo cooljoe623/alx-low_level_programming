97ca20cb0384080f71f3c07a8b1765634b8cd881
Your Name <you@example.com>
2023-05-04 10:52:57 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,21 @@
-#include <stdio.h>
-
-/**
- * the function converts binary to unsigned integer
- * b = pointing to string char 0 and 1
- * return, the converted number that is not 0,1
- *
- * */
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b)
 {
     unsigned int num = 0;
     
-    if (b == NULL) {
+    if (b == NULL)
         return 0;
-    }
-    
-    while (*b != '\0') {
-        if (*b == '0') {
-            num = (num << 1) | 0;
-        } else if (*b == '1') {
+
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] == '0')
+            num = num << 1;
+        else if (b[i] == '1')
             num = (num << 1) | 1;
-        } else {
-         
-	       	return 0;
-        }
-        
-        b++;
+        else
+            return 0;
     }
-    
+
     return num;
 }
