c6b8da9cdd012bd20ce37f3381965a8c662889ec
Your Name <you@example.com>
2023-05-04 11:11:22 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,25 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: string containing binary number
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a string containing a binary number
  *
- * Return: the converted number, or 0 if there is one or more chars
- * in the string b that is not 0 or 1, or if b is NULL
+ * Return: the converted number, or 0 if there is an error
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int result = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b != '\0')
-	{
+	for (; *b; ++b) {
 		if (*b != '0' && *b != '1')
 			return (0);
-
-		num <<= 1;
-		num += (*b - '0');
-		b++;
+		result <<= 1;
+		if (*b == '1')
+			result += 1;
 	}
 
-	return (num);
+	return (result);
 }
