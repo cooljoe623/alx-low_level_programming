27dfd9893c37b45859de70a50bb94e1864c2cfdf
Your Name <you@example.com>
2023-05-04 22:26:00 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,32 @@
  *
  * Return: the converted number, or 0 if there is an error
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int qw;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (; *b; ++b) {
-		if (*b != '0' && *b != '1')
+	qw = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		result <<= 1;
-		if (*b == '1')
-			result += 1;
+		}
+
+		if (b[len] & 1)
+		{
+			qw += base_two;
+		}
 	}
 
-	return (result);
+	return (qw);
 }
