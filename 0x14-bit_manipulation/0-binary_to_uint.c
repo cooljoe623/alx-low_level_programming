246809635abdce5738409a208dbb8ace38aab379
Your Name <you@example.com>
2023-05-04 22:41:24 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,32 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: a string containing a binary number
- *
- * Return: the converted number, or 0 if there is an error
- */
-
+* binary_to_uint - Converts a binary number to an unsigned int.
+* @b: A pointer to a string of 0 and 1 chars.
+*
+* Return: If b is NULL or contains chars not 0 or 1 - 0.
+*         Otherwise - the converted number.
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int qw;
-	int len, base_two;
+unsigned int num = 0, mult = 1;
+int len;
 
-	if (!b)
-		return (0);
+if (b == NULL)
+return (0);
 
-	qw = 0;
+for (len = 0; b[len];)
+len++;
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
+for (len -= 1; len >= 0; len--)
+{
+if (b[len] != '0' && b[len] != '1')
+return (0);
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
+num += (b[len] - '0') * mult;
+mult *= 2;
+}
 
-		if (b[len] & 1)
-		{
-			qw += base_two;
-		}
-	}
-
-	return (qw);
+return (num);
 }
