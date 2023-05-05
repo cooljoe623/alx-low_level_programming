b82690e99ca3520103d248f5267a6cc5c450e4b3
Your Name <you@example.com>
2023-05-04 22:55:04 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: number to print
- *
- * Return: void
- */
-void print_binary(unsigned long int n)
+* get_endianness - Checks the endianness.
+*
+* Return: If big-endian - 0.
+*         If little-endian - 1.
+*/
+int get_endianness(void)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+int num = 1;
+char *endian = (char *)&num;
+
+if (*endian == 1)
+return (1);
+
+return (0);
 }
