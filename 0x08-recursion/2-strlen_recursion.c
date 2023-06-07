e9555adf2e861b4a8a74e9973d83db2bae3f394e
COOL JOE623 <125765098+cooljoe623@users.noreply.github.com>
2023-06-07 11:19:20 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /*
- * _strlen_recursion:returns the length of a string
+ * _strlen_recursion(char *s):returns the length of a string
  * @s:length of a string
  *
  * Return (0) always
