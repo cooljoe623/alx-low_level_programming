b487742f04319434aef8a82baadb86aed1427ad4
COOL JOE623 <125765098+cooljoe623@users.noreply.github.com>
2023-06-07 13:04:17 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
 int _sqrt(int n, int i);
 /*
  * int _sqrt_recursion(int n): computes the natural square root of a number
- * @n: the number 
+ * @n:the number 
  *
  * return: square root of the number
  */
