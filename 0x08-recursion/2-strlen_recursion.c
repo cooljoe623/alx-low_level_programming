09b88c42685299464c5b65090a9ceab35665a074
COOL JOE623 <125765098+cooljoe623@users.noreply.github.com>
2023-06-07 12:00:32 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int n;
-	n = _strlen_recursion(s+1);
-	printf("%d\n",n + 1);
-	
-	if (*s == 0);
-	
-return (0);
+	int n = 0;
+
+	if (*s == 0)
+		return (0);
+
+	n = _strlen_recursion(s + 1);
+
+	return (n + 1);
 }
