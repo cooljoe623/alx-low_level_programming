6a83a025599f18777e753afaf3c6ecee33aebe80
cooljoe623 <josephgichure692@gmail.com>
2023-06-21 20:41:05 +0300

    functional pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "function_pointers.h"
 
-/*
- * main: function that prints a name
- * @s: string 
- *
- * return: name
+/**
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
  */
-void print_name(char *name, void (*f)(char *));
+
+void print_name(char *name, void (*f)(char *))
 {
-	int main(){
-		printf("%s\n",name);
+	if (name && f)
+	{
+		f(name);
 	}
 }
+
