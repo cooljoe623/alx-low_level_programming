8e81913841d8b0fc6af9957edc6f783355ae31c0
COOL JOE623 <125765098+cooljoe623@users.noreply.github.com>
2023-07-11 09:29:59 +0300

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,12 +2,6 @@
 
 #define MAIN_H
 
-
-
-#include <stdio.h>
-
-#include <stdlib.h>
-
 #include <sys/stat.h>
 
 #include <sys/types.h>
@@ -15,7 +9,6 @@
 #include <fcntl.h>
 
 #include <unistd.h>
-
 
 
 int _putchar(char c);
@@ -27,7 +20,5 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 
 int append_text_to_file(const char *filename, char *text_content);
-
-
 
 #endif
